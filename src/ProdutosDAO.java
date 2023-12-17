9a8bc3ace6e827b1ec0049b18063a3cf393b7071
Sunoski <wallace00356@gmail.com>
2023-12-17 11:48:41 -0300

    Botão de Cadastro e Listagem de produtos já em funcionamento

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -10,9 +10,12 @@
 
 import java.sql.PreparedStatement;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class ProdutosDAO {
@@ -24,14 +27,37 @@ public class ProdutosDAO {
     
     public void cadastrarProduto (ProdutosDTO produto){
         
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        try {
+            Statement st;
+            conn = new conectaDAO().connectDB(); // Conecta na DB
+            st = conn.createStatement();
+            String txt;
+            txt = String.format("INSERT INTO `produtos` (`id`, `nome`, `valor`, `status`) VALUES (%s, '%s', %s, '%s');", produto.getId(), produto.getNome(), produto.getValor(), produto.getStatus());
+            st.executeUpdate(txt);
+        } catch (SQLException ex) {
+            System.out.println("Sintaxe de comando invalida ");
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
-        
+        try {
+            Statement st;
+            conn = new conectaDAO().connectDB(); // Conecta na DB
+            st = conn.createStatement();
+            String txt;
+            txt = String.format("select * from produtos");
+            ResultSet rs = st.executeQuery(txt);
+            while (rs.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(rs.getInt("id"));
+                produto.setNome(rs.getString("nome"));
+                produto.setValor(Integer.valueOf(rs.getString("valor")));
+                produto.setStatus(rs.getString("status"));
+                listagem.add(produto);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Sintaxe de comando invalida ");
+        }
         return listagem;
     }
     
